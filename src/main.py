8f37952eb4d6667003c8f8d887407d7c5ea9b000
Danyal <danyalm2002@gmail.com>
2022-08-24 23:23:39 +0500

    added option to enter path and view files

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import os
 FORMATS = {
     'img': ['png', 'jpg', 'jpeg'],
     'video': ['mp4'],
-    'doc': ['doc', 'docx', 'pptx', 'xlxs'],
+    'doc': ['doc', 'docx', 'pptx', 'xlxs', 'pdf', 'html'],
     'audio': ['mp3', 'wav'],
     'misc': [],
     'compressed': ['zip', 'rar']
@@ -15,5 +15,21 @@ def main():
     path = ''
 
     while run:
-        path = input('Input full path of folder: ')
-        sort_mode = input('Input sort mode: ')
+        path = input('Input full path of folder (cd for current directory): ')
+        sort_mode = input(
+            'Input sort mode (1: type, format; 2: type, 3: format, 4: reset) : ')
+
+        if path == 'cd':
+            files = os.listdir()
+        else:
+            files = os.listdir(path)
+
+        for file in files:
+            print(file)
+
+        cont = input('Continue? (y, n): ')
+        if cont == 'n':
+            run = False
+
+
+main()
