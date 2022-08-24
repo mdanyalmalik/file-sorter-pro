985061991fb9578cfb507c0ef9cdee039d9b5be6
Danyal <danyalm2002@gmail.com>
2022-08-24 23:00:21 +0500

    added a rough format list

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+
+FORMATS = {
+    'img': ['png', 'jpg', 'jpeg'],
+    'video': ['mp4'],
+    'doc': ['doc', 'docx', 'pptx', 'xlxs'],
+    'audio': ['mp3', 'wav'],
+    'misc': [],
+    'compressed': ['zip', 'rar']
+}
+
+
+def main():
+    run = True
+    path = ''
+
+    while run:
+        path = input('Input full path of folder: ')
