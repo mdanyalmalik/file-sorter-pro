69f20331b2f7c9b803894ffdcb20f3eaaa4170bf
Danyal <danyalm2002@gmail.com>
2022-09-05 19:22:15 +0500

    added mode 2

--- src/main.py
+++ src/main.py
@@ -15,6 +15,16 @@ def mode_1(path):  # type, format
         path = pathlib.Path().resolve()
     files = os.listdir(path)
 
+    for file in files:
+        ext = os.path.splitext(file)[1][1:]
+
+        for type, exts in FORMATS.items():
+            if ext in exts:
+                os.makedirs(os.path.join(path, type))
+                os.makedirs(os.path.join(path, type, ext))
+                os.rename(os.path.join(path, file),
+                          os.path.join(path, type, ext, file))
+
 
 def mode_2(path):  # type
     if path == 'cd':
@@ -31,6 +41,14 @@ def mode_2(path):  # type
                           os.path.join(path, type, file))
 
 
+def mode_3(path):
+    pass
+
+
+def mode_4(path):
+    pass
+
+
 if __name__ == '__main__':
     run = True
 
@@ -39,8 +57,14 @@ if __name__ == '__main__':
         sort_mode = input(
             'Input sort mode (1: type, format; 2: type, 3: format, 4: reset) : ')
 
-        if int(sort_mode) == 2:
+        if int(sort_mode) == 1:
+            mode_1(path)
+        elif int(sort_mode) == 2:
             mode_2(path)
+        elif int(sort_mode) == 3:
+            mode_3(path)
+        elif int(sort_mode) == 4:
+            mode_4(path)
 
         cont = input('Continue? (y, n): ')
         if cont == 'n':
